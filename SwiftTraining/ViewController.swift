ff19d833bbc898835b60bb7c30c80d415e63f860
yuuki-noda <yuki.noda.mail@gmail.com>
2024-11-25 18:20:27 +0900

    add xcode project

---
+++ SwiftTraining/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftTraining
+//
+//  Created by yuki noda on 2024/11/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
